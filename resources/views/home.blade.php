cfc3b83e90245a72e43cfd53491625772cebde23
heyddar <afshar13ali@gmail.com>
2020-08-17 11:12:44 +0430

    edit title#2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>سایت شخصی علی حیدری</title>
+    <title>سایت شخصی علی حیدری افشار</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="سایت شخصی علی حیدری" />
     <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, Material CV, portfolio" />
